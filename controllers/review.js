31db76a75d44d5133f30f7332ef0968e245bf439
Vaibhav <vaibhav.ranshoor1155@gmail.com>
2023-03-21 17:38:16 +0530

    update comments from controllers

--- controllers/review.js
+++ controllers/review.js
@@ -1,20 +1,9 @@
-// Importing model:
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 
 module.exports.createReview = async (req, res) => {
-  // only for debugging
-  // res.send(req.body);
-
-  // structure of review object:
-  //{"review":{"rating":"2","body":"testing review"}}
-
-  // notice that you have to await findById!
-  // console.log(req.params.id);
-
   const campground = await Campground.findById(req.params.id);
   const review = new Review(req.body.review);
-  // reviews will have reviewAuthors same way campgrounds have authors:
   review.author = req.user._id;
 
   campground.reviews.push(review);
@@ -28,8 +17,6 @@ module.exports.createReview = async (req, res) => {
 
 module.exports.deleteReview = async (req, res) => {
   const { id, reviewId } = req.params;
-
-  //The $pull operator removes from an existing array all instances of a value or values that match a specified condition.
   const cammpground = await Campground.findByIdAndUpdate(id, {
     $pull: { reviews: reviewId }
   });
